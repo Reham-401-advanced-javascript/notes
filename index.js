e3f007b5a93c8bfb5067dde75240cb86547d688f
Reham-Omar <rehamalsobh96@gmail.com>
2020-05-20 10:34:32 +0300

    still

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
-
+require('dotenv').config();
+const mongoose = require('mongoose');
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
+
+const mongo = process.env.MONGODB_URI;
+mongoose.connect(mongo, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const addNewNote = new Input();
 // console.log('input note',addNewNote);
